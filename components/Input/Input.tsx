964324efeaa5cb5cc6bcfc01369a1adb273b0529
manuhutu344 <nandoperdos2@gmail.com>
2023-10-08 20:46:09 +0900

    Menginstall tailwind forms

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client'
 
-function Input() {
+import React from 'react'
+import clsx from 'clsx'
+import {FieldErrors, FieldValues, UseFormRegister} from 'react-hook-form'
+
+interface Props{
+    label: string
+    id: string
+    type?: string
+    required?: boolean
+    register: UseFormRegister<FieldValues>,
+    errors: FieldErrors,
+    disabled?: boolean
+}
+
+function Input ({label, id, type, required, register, errors, disabled}:Props) {
   return (
-    <div>Input</div>
+    <div>
+        <label htmlFor={id} className='block text-sm font-medium leading-6 text-gray-900'>
+            {label}
+        </label>
+    </div>
   )
 }
 
