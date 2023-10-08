997768716be2c9f2eebf95966d92b9ebd7171cf3
manuhutu344 <nandoperdos2@gmail.com>
2023-10-08 20:29:53 +0900

    Membuat Componen input

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react'
 import {useState, useCallback} from 'react'
+import { useForm, FieldValues, SubmitHandler } from 'react-hook-form'
+import Input from './Input/Input'
 
 type Variant = 'LOGIN' | 'REGISTER'
 
@@ -11,10 +13,47 @@ function Authform() {
     const toggleVariant = useCallback(()=>{
         if(variant === 'LOGIN'){
             setVariant('REGISTER')
+        }else{
+          setVariant('LOGIN')
         }
-    }, [])
+    }, [variant])
+
+    const {
+      register,
+      handleSubmit,
+      formState: {
+        errors
+      }
+    } = useForm<FieldValues>({
+      defaultValues: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    })
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) =>{
+      setIsloading(true)
+      if(variant === 'REGISTER'){
+
+      }
+      if(variant === 'LOGIN'){
+
+      }
+    }
+
+    function socialAction(action: string){
+      setIsloading(true)
+    }
+
   return (
-    <div>Authform</div>
+    <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+        <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
+          <Input />
+        </form>
+      </div>
+    </div>
   )
 }
 
