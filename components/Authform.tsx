9cad01e480acd95232a0ab22c528540998a3807f
manuhutu344 <nandoperdos2@gmail.com>
2023-10-09 20:29:51 +0900

    Membuat form input menggunakan tailwindcss forms

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -50,7 +50,11 @@ function Authform() {
     <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
       <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
         <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
-          <Input label='Nando Saja'/>
+          {variant === 'REGISTER' && (
+            <Input id="name" label='Nama' errors={errors} register={register}/>
+          )}
+          <Input id="email" label='Masukan Email Anda' type='email' errors={errors} register={register}/>
+          <Input id="password" label='Masukan password anda' type='password' errors={errors} register={register}/>
         </form>
       </div>
     </div>
