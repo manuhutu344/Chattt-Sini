e94e0e4207d3ed16f3629c08633f7571e3db56ab
manuhutu344 <nandoperdos2@gmail.com>
2023-10-10 20:22:29 +0900

    Membuat untuk masuk akun google dan lain-lain

--- components/Authform.tsx
+++ components/Authform.tsx
@@ -57,9 +57,21 @@ function Authform() {
           <Input id="email" label='Masukan Email Anda' type='email' errors={errors} register={register}/>
           <Input id="password" label='Masukan password anda' type='password' errors={errors} register={register}/>
           <div>
-            <Button>Pencet Untuk Login</Button>
+            <Button disabled={isLoading} fullwidth type='submit'>{variant === 'LOGIN' ? 'Sign in' : 'Register'}</Button>
           </div>
         </form>
+        <div className='mt-6'>
+          <div className='relative'>
+            <div className='absolute inset-0 flex items-center'>
+              <div className='w-full border-t border-gray-300' />
+            </div>
+            <div className='relative flex justify-center text-sm'>
+              <span className='bg-white px-2 text-gray-500'>
+                Atau Masuk Dengan
+              </span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
