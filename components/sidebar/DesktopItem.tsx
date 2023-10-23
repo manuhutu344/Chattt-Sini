5f5922811fabe3b9955c7a6c163cc4f19b901742
manuhutu344 <nandoperdos2@gmail.com>
2023-10-23 09:43:11 +0900

    Menstyle icon

--- components/sidebar/DesktopItem.tsx
+++ components/sidebar/DesktopItem.tsx
@@ -20,9 +20,9 @@ function DesktopItem({label, icon: Icon, href, onClick, active}:Props) {
     }
   return (
     <li onClick={handleClick}>
-        <Link href={href}>
+        <Link href={href} className={clsx(`group flex gap-x-3 rounded-md p-3 text-sm leading-6 font-semibold text-gray-500 hover:text-black hover:bg-gray-100`, active && 'bg-gray-100 text-black')}>
             <Icon className='h-6 w-6 shrink-0' />
-        <span>
+        <span className='sr-only'>
             {label}
         </span>
         </Link>
