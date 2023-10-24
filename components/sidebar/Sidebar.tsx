d9127659eb7432f9b4fee424b744747cca6a9c0a
manuhutu344 <nandoperdos2@gmail.com>
2023-10-24 18:51:07 +0900

    menggunakan next-superjson-plugin

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import DesktopSidebar from './DesktopSidebar'
 import MobileFooter from './MobileFooter'
+import getCurrentUser from '@/app/actions/getCurrentUser'
 
 interface Props{
     children: React.ReactNode
 }
 
 async function Sidebar({children}:Props) {
+  const currentUser = await getCurrentUser()
   return (
     <div className='h-full'>
-        <DesktopSidebar />
+        <DesktopSidebar currentUser={currentUser} />
         <MobileFooter />
         <main className='lg:pl-20 h-full'>
             {children}
