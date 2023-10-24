57127adda307a739b23a8d65ec3c0d47fee0a823
manuhutu344 <nandoperdos2@gmail.com>
2023-10-24 19:31:11 +0900

    Membuat avatar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -11,7 +11,7 @@ async function Sidebar({children}:Props) {
   const currentUser = await getCurrentUser()
   return (
     <div className='h-full'>
-        <DesktopSidebar currentUser={currentUser} />
+        <DesktopSidebar currentUser={currentUser!} />
         <MobileFooter />
         <main className='lg:pl-20 h-full'>
             {children}
--- next.config.js
+++ next.config.js
@@ -5,6 +5,13 @@ const nextConfig = {
         swcPlugins: [
             ["next-superjson-plugin", {}]
         ]
+    },
+    images:{
+        domains: [
+            "res.cloundinary.com",
+            "avatars.githubusercontent.com",
+            "lh3.googleusercontent.com"
+        ]
     }
 }
 
