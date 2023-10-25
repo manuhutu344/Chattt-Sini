7872812791af508731e791b0e5a80b50c860859c
manuhutu344 <nandoperdos2@gmail.com>
2023-10-25 09:21:25 +0900

    Membuat warna hijau sebagai penanda online

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -14,6 +14,7 @@ function Avatar({user}:Props) {
         <div className='relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11'>
             <Image alt='Avatar' src={user?.image || '/image/placeholder.jpg'}  fill/>
         </div>
+        <span className='absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3' />
     </div>
   )
 }
