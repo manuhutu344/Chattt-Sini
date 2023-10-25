d4b704653b5313bb16ea08fc239d1bc8fe999732
manuhutu344 <nandoperdos2@gmail.com>
2023-10-25 10:05:05 +0900

    Membuat user list

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,4 +1,6 @@
 import Sidebar from "@/components/sidebar/Sidebar"
+import getUsers from "../actions/getUsers"
+import UserList from "@/components/UserList"
 
 interface Props{
     children: React.ReactNode
@@ -7,9 +9,11 @@ interface Props{
 export default async function UsersLayout({
     children
 }:Props){
+    const users = await getUsers()
     return(
         <Sidebar>
         <div className="h-full">
+            <UserList items={users} />
             {children}
         </div>
         </Sidebar>
