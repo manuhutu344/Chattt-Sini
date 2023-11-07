d329e6c20e71ebbc6faead769391ff0970cbdf22
manuhutu344 <nandoperdos2@gmail.com>
2023-11-07 20:53:11 +0900

    Membuat useeffect untuk body di messages

--- components/Body.tsx
+++ components/Body.tsx
@@ -1,9 +1,10 @@
 "use client"
 import useConversation from '@/app/hooks/useConversation'
 import { FullMessageType } from '@/app/types'
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useState, useRef} from 'react'
 import MessageBox from './MessageBox'
+import axios from 'axios'
 
 interface Props{
   initialMessages: FullMessageType[]
@@ -12,8 +13,12 @@ interface Props{
 function Body({initialMessages}:Props) {
   const [messages, setMessages] = useState(initialMessages)
   const bottomRef = useRef<HTMLDivElement>(null)
-
   const {conversationId} = useConversation()
+
+  useEffect(()=>{
+    axios.post(`/api/conversations/${conversationId}/seen`)
+  },[conversationId])
+
 
   return (
     <div className='flex-1 overflow-y-auto'>
