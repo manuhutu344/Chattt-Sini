f936dca2258c34212a97554695f9742e6dd0f373
manuhutu344 <nandoperdos2@gmail.com>
2023-11-22 20:03:03 +0900

    Membuat Ui Tombol dan navigasi saat menghapus pesan

--- components/Input/ProfileDrawer.tsx
+++ components/Input/ProfileDrawer.tsx
@@ -7,7 +7,7 @@ import {format} from 'date-fns'
 import { Transition, Dialog } from '@headlessui/react'
 import {IoClose, IoTrash} from 'react-icons/io5'
 import Avatar from '../Avatar'
-import Modal from '../Modal'
+import ConfirmModal from '../ConfirmModal'
 
 interface Props{
     isOpen: boolean
@@ -19,7 +19,7 @@ interface Props{
 
 function ProfileDrawer({isOpen, onClose, data}:Props) {
     const otherUser = useOtherUser(data)
-    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [confirmOpen , setConfirmOpen] = useState(false)
     const joinedDate = useMemo(()=>{
         return format(new Date(otherUser.createdAt), 'PP')
     }, [otherUser.createdAt])
@@ -34,11 +34,7 @@ function ProfileDrawer({isOpen, onClose, data}:Props) {
     }, [data])
   return (
     <>
-    <Modal isOpen={isModalOpen} onClose={()=>setIsModalOpen(false)}>
-        <div className='bg-white p-5'>
-            <p>Hay Bang</p>
-        </div>
-    </Modal>
+    <ConfirmModal isOpen={confirmOpen} onClose={()=>setConfirmOpen(false)} />
     <Transition.Root show={isOpen} as={Fragment}>
         <Dialog as="div" className='relative z-50' onClose={onClose}>
             <Transition.Child as={Fragment} enter="ease-out duration-500" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-500" leaveFrom="opacity-100" leaveTo="opacity-0">
@@ -72,7 +68,7 @@ function ProfileDrawer({isOpen, onClose, data}:Props) {
                                                     {statusText}
                                                 </div>
                                                 <div className='flex gap-10 my-8'>
-                                                    <div onClick={()=>setIsModalOpen(true)} className='flex flex-col gap-3 items-center cursor-pointer hover:opacity-75'>
+                                                    <div onClick={()=>setConfirmOpen(true)} className='flex flex-col gap-3 items-center cursor-pointer hover:opacity-75'>
                                                         <div className='w-10 h-10 bg-neutral-100 rounded-full flex items-center justify-center'>
                                                             <IoTrash size={20} />
                                                         </div>
