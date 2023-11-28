4c8242223d983fed894e1de6550af2aeffe98248
manuhutu344 <nandoperdos2@gmail.com>
2023-11-28 10:50:04 +0900

    Membuat Upload Gambar dan save perubahan

--- components/SettingsModal.tsx
+++ components/SettingsModal.tsx
@@ -7,6 +7,10 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
 import axios from 'axios'
 import toast from 'react-hot-toast'
 import Modal from './Modal'
+import Input from './Input/Input'
+import Image from 'next/image'
+import { CldUploadButton } from 'next-cloudinary'
+import Button from './Input/Button'
 
 interface Props{
     isOpen?: boolean
@@ -49,9 +53,36 @@ function SettingsModal({isOpen, onClose, currentUser}:Props) {
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className='space-y-12'>
                 <div className='border-b border-gray-900/10 pb-12'>
-                    <h2 className='text-base font-semibold'>
+                    <h2 className='text-base font-semibold leading-7 text-gray-900'>
                         Profil
                     </h2>
+                    <p className='mt-1 text-sm leading-6 text-gray-600'>
+                        Tuliskan Informasi Tentang Anda
+                    </p>
+                    <div className='mt-10 flex flex-col gap-y-8'>
+                        <Input disabled={isLoading} label='name' id='name' errors={errors} required register={register} />
+                        <div>
+                            <label className='block text-sm font-medium leading-6 text-gray-900'>
+                                Foto
+                            </label>
+                            <div className='mt-2 flex items-center gap-x-3'>
+                                <Image width={48} height={48} className='rounded-full' src={image || currentUser?.image || '/image/placeholder.jpg'} alt='Avatar' />
+                                <CldUploadButton options={{maxFiles: 1}} onUpload={handleUpload} uploadPreset='kczgrefy'>
+                                    <Button disabled={isLoading} secondary type='button'>
+                                        Rubah Gambar Anda
+                                    </Button>
+                                </CldUploadButton>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='mt-6 flex items-center justify-end gap-x-6'>
+                    <Button disabled={isLoading} secondary onClick={onClose}>
+                        Tidak Jadi
+                    </Button>
+                    <Button disabled={isLoading} type='submit'>
+                        Iya Jadi
+                    </Button>
                 </div>
             </div>
         </form>
