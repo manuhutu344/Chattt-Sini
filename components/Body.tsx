593ce3ece2bc2f93120020aa0d7451657355639d
manuhutu344 <nandoperdos2@gmail.com>
2023-12-08 21:07:08 +0900

    Membuat load otomtis menggunakan pusher

--- components/Body.tsx
+++ components/Body.tsx
@@ -5,6 +5,8 @@ import React, { useEffect } from 'react'
 import {useState, useRef} from 'react'
 import MessageBox from './MessageBox'
 import axios from 'axios'
+import { pusherClient } from '@/app/libs/pusher'
+import { find } from 'lodash'
 
 interface Props{
   initialMessages: FullMessageType[]
@@ -18,6 +20,27 @@ function Body({initialMessages}:Props) {
   useEffect(()=>{
     axios.post(`/api/conversations/${conversationId}/seen`)
   },[conversationId])
+
+  useEffect(()=>{
+    pusherClient.subscribe(conversationId)
+    bottomRef?.current?.scrollIntoView()
+    const messageHandler = (message: FullMessageType)=>{
+      axios.post(`/api/conversations/${conversationId}/seen`)
+
+      setMessages((current)=>{
+        if(find(current, {id: message.id})){
+          return current
+        }
+        return[...current, message]
+      })
+      bottomRef?.current?.scrollIntoView()
+    }
+    pusherClient.bind('messages:new', messageHandler)
+    return () =>{
+      pusherClient.unsubscribe(conversationId)
+      pusherClient.unbind('messages:new', messageHandler)
+    }
+  }, [conversationId])
 
 
   return (
