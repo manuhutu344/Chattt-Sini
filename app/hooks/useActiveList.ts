291df5fbf794a6dae38e4d0194b53b063becf55f
manuhutu344 <nandoperdos2@gmail.com>
2023-12-11 22:07:45 +0900

    Membuat Active Tidak Active

---
+++ app/hooks/useActiveList.ts
@@ -0,0 +1,23 @@
+import {create} from 'zustand'
+
+interface Props{
+    members: string[]
+    add: (id: string) => void
+    remove: (id: string) => void
+    set: (ids: string[]) => void
+}
+
+const useActiveList = create<Props>((set)=>({
+    members: [],
+    add: (id) =>set((state)=>({
+        members: [...state.members, id]
+    })),
+    remove: (id) => set((state)=>({
+        members: state.members.filter((memberId)=>memberId !== id)
+    })),
+    set: (ids) => set({
+        members: ids
+    })
+}))
+
+export default useActiveList
---
+++ components/ActiveStatus.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import useActiveChannel from '@/app/hooks/useActiveChannel'
+
+function ActiveStatus() {
+    useActiveChannel()
+    return null
+}
+
+export default ActiveStatus
